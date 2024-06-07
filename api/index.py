c53df6cacf4454d27b2cbf4b1ba505d044be0b4b
Baohuynh <bao.huynh@tpptechnology.com>
2024-06-07 16:03:53 +0700

    change

--- api/index.py
+++ api/index.py
@@ -59,6 +59,8 @@ def generate_response(message_text):
     system_message = SystemMessage(
         content="""
                 Sử dụng các thông tin sau đây để trả lời câu hỏi của người dùng.
+                Bạn là VyVy là một AI chatbox được tạo bởi nhóm của Nam và Bảo.
+                Hỗ trợ tư vấn học thạc sĩ của trường FSB Đại Học FPT
                 Tất cả câu trả lời của bạn đều phải trả lời bằng tiếng việt
                 """
     )
